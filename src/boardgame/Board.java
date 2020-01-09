ce4334b175539c80be118dd50b63408a79991369
Karl Gama <kaiquesilva49@hotmail.com>
2020-01-09 13:15:30 -0300

    Place pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -31,9 +31,16 @@ public class Board {
 	{
 		return pieces[row][column];
 	}
+	
 	public Piece piece(Position position) 
 	{
 		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) 
+	{
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 	
 }
